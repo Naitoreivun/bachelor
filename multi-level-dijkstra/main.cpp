27587d8fbbd612492f54938b492d1c1b2ef1ba01
Naitoreivun <dominik646@gmail.com>
2018-06-26 01:04:24 +0200

    add small benchmark; move multi level dijkstra to subdir

--- multi-level-dijkstra/main.cpp
+++ multi-level-dijkstra/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <chrono>
 #include "MultilevelGraph.h"
 
 int n, m;
@@ -15,6 +16,10 @@ vector<Vertex *> getVerticesSortedByDegree();
 bool calculateDistance(int v1, int v2, MultilevelGraph &M);
 
 void calculate(MultilevelGraph &M);
+
+void benchmark(MultilevelGraph &M);
+
+using namespace std::chrono;
 
 int main() {
     cout << "start" << endl;
@@ -41,13 +46,15 @@ int main() {
 //    calculateDistance(7, 5, M);
 //    calculateDistance(5, 7, M);
 //    calculateDistance(1, 500, M);
-//
-    calculate(M);
+
+//    calculate(M);
 
 //    calculateDistance(1, 39, M); // USairport500.in airportDebug2
 //    calculateDistance(4, 3, M); // airportDebug2
 //    calculateDistance(4, 254, M); // USairport500.in
 //    calculateDistance(32, 440, M);
+
+    benchmark(M);
 
     cout << "finish" << endl;
     return 0;
@@ -65,15 +72,15 @@ void calculate(MultilevelGraph &M) {
 }
 
 void init() {
-//    const char *const filename = "../tests/airportDebug.in";
-//    const char *const filename = "../tests/airportDebug2.in";
-//    const char *const filename = "../tests/airportDebug3.in";
-    const char *const filename = "../tests/USairport500.in";
-//    const char *const filename = "../tests/test.in";
-//    const char *const filename = "../tests/test2.in";
-//    const char *const filename = "../tests/test3.in";
-//    const char *const filename = "../tests/test4.in";
-//    const char *const filename = "../tests/pile.in";
+//    const char *const filename = "../../tests/airportDebug.in";
+//    const char *const filename = "../../tests/airportDebug2.in";
+//    const char *const filename = "../../tests/airportDebug3.in";
+    const char *const filename = "../../tests/USairport500.in";
+//    const char *const filename = "../../tests/test.in";
+//    const char *const filename = "../../tests/test2.in";
+//    const char *const filename = "../../tests/test3.in";
+//    const char *const filename = "../../tests/test4.in";
+//    const char *const filename = "../../tests/pile.in";
     fstream file(filename);
 
     if (file.good()) {
@@ -134,4 +141,33 @@ bool calculateDistance(int v1, int v2, MultilevelGraph &M) {
 //        return true;
 //    }
     return multi == reg;
+}
+
+
+void benchmark(MultilevelGraph &M) {
+//    milliseconds ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+//    cout << (ms.count()) << endl;
+
+    cout << "reg start\n";
+    milliseconds startReg = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+    for (int i = 1; i <= n/5; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            M.regularDijkstra(originalVertices[i - 1], originalVertices[j - 1]);
+        }
+        cout << i  << " ";
+    }
+    milliseconds stopReg = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+    cout << "\nreg stop:\n\t" << (stopReg.count() - startReg.count()) << "\n\n";
+
+    cout << "mul start\n";
+    milliseconds startMul = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+    for (int i = 1; i <= n/5; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            M.calculateDistance(originalVertices[i - 1], originalVertices[j - 1]);
+        }
+        cout << i  << " ";
+    }
+    milliseconds stopMul = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+    cout << "\nmul stop:\n\t" << (stopMul.count() - startMul.count()) << "\n\n";
+
 }
